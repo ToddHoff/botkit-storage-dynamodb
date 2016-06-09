15479b63bee3c6d9a1a0c768b60b16b63203c327
Todd Hoff <toddhoffious@gmail.com>
2016-06-09 14:59:24 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# botkit-storage-dynamodb
+
+A DynamoDB storage module for Botkit.
+
+## Usage
+
+You should have three tables already created in your DynamoDB database: namespace + teams, namespace + users and namespace + channels.
+
+Namespace is the namespace passed into the module. If the namespace is, for example, "testSlack" then the table's you'll want to precreate are: testSlackteams, testSlackusers, testSlackchannels.
+
+Each table should configure 'id' as the primary key.
+
+Passing AWS into the module is optional. If it's not passed in it will be created. If you need to do use a custom configuration you can pass AWS in. 
+
+
+```
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('awscfg.json');
+
+var Dynamo_Store = require('botkit-storage-dynamodb');
+var dynamo_store = new Dynamo_Store({AWS: AWS, namespace: "testSlack"});
+
+var controller = Botkit.slackbot({
+  storage: dynamo_store
+}).configureSlackApp(
+  {
+    clientId: appcfg.Slack.clientId,
+    clientSecret: appcfg.Slack.clientSecret,
+    scopes: ['commands','bot', 'chat:write:user']
+  }
+);
+```
+
+```
+// then you can use the Botkit storage api, make sure you have an id property
+var beans = {id: 'cool', beans: ['pinto', 'garbanzo']};
+controller.storage.teams.save(beans);
+beans = controller.storage.teams.get('cool');
+
+```
---
+++ src/index.js
@@ -0,0 +1,76 @@
+/**
+ * botkit-storage-dynamodb - DynamoB driver for Botkit
+ *
+ * @param  {Object} config May contain a configured AWS aws-sdk object.
+ * @return {Object} A storage object conforming to the Botkit storage interface
+ */
+module.exports = function(config) {
+  config = config || {};
+  config.namespace = config.namespace || 'botkit';
+  config.AWS = config.AWS || require('aws-sdk');
+
+  var storage = {};
+  var client = new config.AWS.DynamoDB.DocumentClient();
+
+  ['teams', 'channels', 'users'].forEach(function(method) {
+    storage[method] = getStorageObj(client, config.namespace +  method); 
+  })
+
+  return storage;
+};
+
+
+/**
+ * Function to generate a storage object for a given namespace
+ *
+ * @param {Object} client The DynamoDB client
+ * @param {String} namespace The namespace to use for storing in DynamoDB
+ * @returns {{get: get, save: save, all: all, allById: allById}}
+ */
+function getStorageObj(client, namespace) {
+  return {
+    get: function(id, cb) {
+      var params = {
+        TableName: namespace, 
+        Key: { "id": id }
+      }
+
+      client.get(params, function(error, data) {
+        cb(error, (data.Item) ? data.Item: undefined);
+      })
+    },
+    save: function(object, cb) {
+      if (!object.id) {
+        return cb(new Error('The given object must have an id property namespace: ' + namespace + " object:" + JSON.stringify(object)), {});
+      }
+
+      var params = {
+        TableName: namespace, 
+        Item: object
+      }
+
+      client.put(params, function(error, data) {
+        cb(error, data);
+      })
+    },
+    remove: function(id, cb) {
+      var params = {
+        TableName: namespace, 
+        Key: { "id": id }
+      }
+
+      client.delete(params, function(error, data) {
+        cb(error, data);
+      })
+    },
+    all: function(cb, options) {
+      client.scan({
+        TableName : namespace 
+      }, function(error, data) {
+        if (error) return cb(error, null); 
+        cb(null, data.Items);
+      })// scan
+    }
+  }// return
+
+}// getStorageObj
